0150d97336be44a56b5e6e5ab315e670428597f6
Salvador De la T <stijera@MacBook-Pro-de-stijera.local>
2024-02-07 11:37:03 -0600

    correcione sy terminacion del juego

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { useState } from "react";
 
+//?---------------COMPONENTE SQUARE---------------------
 function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
@@ -10,15 +11,14 @@ function Square({ value, onSquareClick }) {
   );
   //onSquareClick => función del componente, al componente Square, y Square llamará a esa función cuando se haga clic en un cuadrado. Comienza con la función que llamará el componente Square cuando se haga clic en él
 }
-const App = ( xIsNext, squares, onPlay ) => {
+
+//?---------------COMPONENTE APP---------------------
+const App = ({xIsNext, squares, onPlay}) => {
 //Cada vez que un jugador se mueve, xIsNext (un valor booleano) se invertirá para determinar qué jugador es el siguiente y se guardará el estado del juego. Actualiza la función handleClick de Board para cambiar el valor de xIsNext:
   //const[xIsNext, setXIsNext] = useState(true);
   //const[squares, setSquares] = useState(Array(9).fill(null))
+  function handleClick(i){ //handkleclick actualiza la matriz squares que contiene el estado de tu tablero,crea una copia de la matriz squares (nextSquares) con el método JavaScript slice() Array. Luego, handleClick actualiza la matriz nextSquares para agregar X al primer cuadrado (índice [0]).
 
-
-
-  //handkleclick actualiza la matriz squares que contiene el estado de tu tablero,crea una copia de la matriz squares (nextSquares) con el método JavaScript slice() Array. Luego, handleClick actualiza la matriz nextSquares para agregar X al primer cuadrado (índice [0]).
-  function handleClick(i){
   // squares[i] Verifica si el cuadrado ya tiene una X o una O. Si el cuadrado ya está lleno, genera un return en la función handleClick, antes de que intente actualizar el estado del tablero.
     if (calculateWinner(squares) || squares[i]) {// calculateWinner(squares)) para comprobar si un jugador ha ganado
       return;
@@ -48,13 +48,15 @@ const App = ( xIsNext, squares, onPlay ) => {
 
   return (
     <>
-
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]}onSquareClick={()=>handleClick(0)}/>
         <Square value={squares[1]}onSquareClick={()=> handleClick(1)} />
         <Square value={squares[2]}onSquareClick={()=>handleClick(2)} />
       </div>
-{/* handleClick(0) es una función de flecha, Cuando se hace clic en el cuadrado, se ejecutará el código después de la «flecha» =>, llamando a handleClick(). */}
+
+{/* handleClick(0) Cuando se hace clic en el cuadrado, se ejecutará el código después de la «flecha» =>, llamando a handleClick(). */}
+
       <div className="board-row">
         <Square value={squares[3]}onSquareClick={()=>handleClick(3)} />
         <Square value={squares[4]}onSquareClick={()=>handleClick(4)} />
@@ -66,33 +68,64 @@ const App = ( xIsNext, squares, onPlay ) => {
         <Square value={squares[7]}onSquareClick={()=>handleClick(7)} />
         <Square value={squares[8]}onSquareClick={()=>handleClick(8)} />
       </div>
-      <div className="status">{status}</div>
+      
     </>
   );
 };
+
+//?---------------COMPONENTE GAME---------------------
+
 export default function Game(){
 
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-//Array(9).fill(null) crea una matriz con nueve elementos y establece cada uno de ellos en null. La llamada useState() a su alrededor declara una variable de estado squares que inicialmente se establece en esa matriz. Cada entrada en la matriz corresponde al valor de un cuadrado.//
-  const currentSquares = history[history.length - 1];
+  const [history, setHistory] = useState([Array(9).fill(null)]);//Array(9).fill(null) crea una matriz con nueve elementos y establece cada uno de ellos en null. La llamada useState() a su alrededor declara una variable de estado squares que inicialmente se establece en esa matriz. Cada entrada en la matriz corresponde al valor de un cuadrado.//
+  const [currentMove, setCurrentMove] = useState(0);// lleva la cuenta del paso que el usuario está viendo en ese momento y lleva por defecto el numeor 0.
+  const xIsNext = currentMove % 2 === 0;
+
+  const currentSquares = history[currentMove];//modifica el componente Game para representar el movimiento seleccionado actualmente, en lugar de representar siempre el movimiento final.
+
+//! funcion handlePlay
   function handlePlay(nextSquares){
 //[...history, nextSquares] crea una nueva matriz que contiene todos los elementos en history, seguido de nextSquares. (Puedes leer la sintaxis extendida ...history como «enumerar todos los elementos en history».)
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    setCurrentMove(nextHistory.length - 1);
+  //Cada vez que se realiza un movimiento, es necesario actualizar currentMove para que apunte a la última entrada del historial.  
   }
+
+//! funcion jumpto
+  // jumpTo actualiza currentMOve y establece xIsNext en true si el número al que estás cambiando currentMove es par.
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+  }
+
+//! funcion moves
+  const moves = history.map((squares, move)=>{
+    let description;
+    if (move > 0) {
+      description = "Ir al movieminto # " + move;
+    }else{
+      description = "Ir al inicio del juego";
+    }
+    return(
+      <li key={move}>
+        <button onClick={()=>jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
   return(
     <div className="game">
       <div className="game-board">
         <App xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
 }
 
+//?---------------TABLERO DEL JUEGO---------------------
 // calculateWinner que toma una matriz de 9 cuadrados, busca un ganador y devuelve 'X', 'O' o null según corresponda
 function calculateWinner(squares) {
   const lines = [
